df57842475cd68823513ff80b8f3f6d506fea05f
Nick <niiickolay@gmail.com>
2021-02-16 18:47:41 +0200

    Add react semantic ui

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import AppRouter from '../components/AppRouter/AppRouter';
 import store from './store';
+import 'semantic-ui-css/semantic.min.css';
 
 const App = () => (
   <Provider store={store}>
