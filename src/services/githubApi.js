7754d150d761f8618b5f91dcd90330ed49381e75
Nick <niiickolay@gmail.com>
2021-02-16 23:34:09 +0200

    Replace mock data with requests to api

--- src/services/githubApi.js
+++ src/services/githubApi.js
@@ -1,18 +1,10 @@
 import axios from 'axios';
 import parseLink from 'parse-link-header';
-// import users from './users.json';
-import userData from './mojombo.json';
 
 const GITHUB_API_URL = 'https://api.github.com/';
 
 axios.defaults.baseURL = GITHUB_API_URL;
 
-// export const getUsers = async ({ page = 1, since = 0 } = {}) => {
-//   console.log(since);
-//   const newUsers = users.slice()
-//     .map((user) => ({ ...user, id: user.id + page * users.length }));
-//   return new Promise((res) => setTimeout(() => res({ data: newUsers, hasMore: true }), 500));
-// };
 export const getUsers = async ({ since = 0, perPage }) => {
   const response = await axios({
     method: 'GET',
@@ -29,4 +21,10 @@ export const getUsers = async ({ since = 0, perPage }) => {
   return { data, hasMore };
 };
 
-export const getUser = async () => Promise.resolve(userData);
+export const getUser = async (username) => {
+  const response = await axios({
+    method: 'GET',
+    url: `/users/${username}`,
+  });
+  return response.data;
+};
